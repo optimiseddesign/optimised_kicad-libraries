56bc8aac78ca702ac7b99904d3398b5ec69cb208
Kevin Bibby <kevin.bibby@icpla.net>
2020-03-17 16:00:58 +0000

    added comment to SmartGit script to help know where to use it

--- environment/smartgit-excel-compare.bat
+++ environment/smartgit-excel-compare.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+rem In Smartgit - in Preferences, add a Diff Tool for e.g. *.xls pattern and set to this script
 rem smartgit diff passes two arguments: old-file, new-file.
 set TEMP_FILE=%TEMP%\smartgit-excel-diff.txt
 ECHO %1 > %TEMP_FILE%
