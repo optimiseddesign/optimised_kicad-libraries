95ef77a013691ad1fd3aa3022dafbac76613378e
Kevin Bibby <kevin.bibby@optimiseddesign.com>
2024-02-19 17:44:48 +0000

    KICAD PCB timelapse script improvements... ...changed to only use commits where kicad_pcb changed, not all ...will also now discover up to max of commit number given ...added silkscreen to layers output ...made SVG opacity variable (was 70% fixed, now 60% variable) ...fix SVG opacity (now ALL objects, text & traces were weird!)

--- environment/scripting/kicad_pcb_timelapse.py
+++ environment/scripting/kicad_pcb_timelapse.py
@@ -11,7 +11,7 @@
 #
 # My initial investigations info for reference;
 #'git show HASH:file/path/name.ext > some_new_name.ext' example to pipe output from a single Git hash to a file
-#'git show branchname~10:file/path/name.ext' example using the branch and relative commit number
+#'git show branchname~10:file/path/name.ext' example using the branch and relative commit number (later changed to use commit hash)
 #'git -C /home/repo' to specify repo location
 # These combined, gives;
 #"C:\Program Files\SmartGit\git\bin\git.exe" -C "C:\freelance\git\pt115a_vrgo-fyt-electronics-main" show miniaturisation~10:./design/pt115a_vrgo-fyt-electronics-main.kicad_pcb > C:\Users\KevinBibby\Desktop\test.kicad_pcb
@@ -37,7 +37,7 @@ CONFIG_GIT_EXE_PATH = "C:\\Program Files\\SmartGit\\git\\bin\\git.exe"
 CONFIG_GIT_REPO_PATH = "C:\\freelance\\git\\pt136a_giraffecctv_edge_controller_generic"
 CONFIG_GIT_PCB_PATH = "./design/pt136a_giraffecctv_edge_controller_generic.kicad_pcb"
 CONFIG_GIT_BRANCH = "master"
-CONFIG_GIT_NUM_COMMITS = 143 #this many commits counting backwards relative to the HEAD, for changes in PCB file only
+CONFIG_GIT_NUM_COMMITS_MAX = 150 #up to this many previous commits (max), for changes in PCB file only
 CONFIG_OUTPUT_PCB_PATH = "C:\\Users\\KevinBibby\\Desktop\\output\\"
 CONFIG_OUTPUT_PCB_PREFIX = "output"
 CONFIG_OUTPUT_IMAGE_PATH = "C:\\Users\\KevinBibby\\Desktop\\output\\"
@@ -51,20 +51,56 @@ CONFIG_OUTPUT_IMAGE_CROP_BOTTOM = 950 #margin from bottom
 CONFIG_OUTPUT_IMAGE_DUPLICATE = 2 # 1 default, 9 max (set higher for duplicates of each one, ensure image interpolation has de-duplication OFF)
 CONFIG_INKSCAPE_PATH = "C:\\Program Files\\Inkscape\\bin\inkscape.com"
 CONFIG_KICAD_CLI_PATH = "C:\\Program Files\\KiCad\\7.0\\bin\\kicad-cli"
-CONFIG_KICAD_LAYERS = "F.Paste,F.Cu,F.Courtyard,In2.Cu,B.Cu,Edge.Cuts"
+CONFIG_KICAD_LAYERS = "F.Silkscreen,F.Paste,F.Cu,F.Courtyard,In2.Cu,B.Cu,Edge.Cuts"
 
 
-# First outputs all the X .kicad_pcb files
-for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
-    print("Exporting KiCAD PCB file from branch '" + CONFIG_GIT_BRANCH + "', commit #" + str(commit_num) + " (" + str(CONFIG_GIT_NUM_COMMITS - commit_num) + " behind HEAD)\n")
+# First outputs all the commit hashes for the kicad_pcb file changes (only where PCB file has changed)
+# git -C repo_path log branch_name --follow -n 10 --pretty=format:%H -- <filename.ext>
+print("Getting Git commit hash list of all KICAD PCB file changes on branch '" + CONFIG_GIT_BRANCH + "', up to " + str(CONFIG_GIT_NUM_COMMITS_MAX) + " commits max...\n")
+
+cmd = [CONFIG_GIT_EXE_PATH,
+        '-C',
+        CONFIG_GIT_REPO_PATH,
+        'log',
+        CONFIG_GIT_BRANCH,
+        '--follow',
+        '-n ' + str(CONFIG_GIT_NUM_COMMITS_MAX),
+        '--pretty=format:%H',
+        '--',
+        CONFIG_GIT_PCB_PATH]
+
+process = subprocess.Popen(args=cmd, 
+                        stdout=subprocess.PIPE,
+                        shell=True, 
+                        universal_newlines=True)
+
+kicad_pcb_hashes = process.stdout.readlines()
+kicad_pcb_hashes_cnt = len(kicad_pcb_hashes)
+
+# Reverse the order (from earliest to latest commit)
+kicad_pcb_hashes.reverse()
+
+# Print a message with the total found and the first 8 characters of the first/last Git commit hashes found
+print("Found " + str(kicad_pcb_hashes_cnt) + " commits, from #" + kicad_pcb_hashes[0][:8] + " (earliest) to #" + kicad_pcb_hashes[kicad_pcb_hashes_cnt - 1][:8] + " (latest)\n")
+
+# Now output all the .kicad_pcb files for these commit hashes
+commit_cnt = 0
+for commit_hash in kicad_pcb_hashes:
+    commit_cnt += 1
+
+    # Strip off any trailing '\n'
+    commit_hash = commit_hash.rstrip('\n')
+
+    # Export KiCAD PCB file for this commit hash
+    print("Exporting KiCAD PCB file for commit #" + commit_hash + " as commit #" + str(commit_cnt) + " ...")
 
     cmd = [CONFIG_GIT_EXE_PATH,
             '-C',
             CONFIG_GIT_REPO_PATH,
             "show",
-            CONFIG_GIT_BRANCH + "~" + str(CONFIG_GIT_NUM_COMMITS - commit_num) + ":" + CONFIG_GIT_PCB_PATH,
+            commit_hash + ":" + CONFIG_GIT_PCB_PATH,
             ">",
-            CONFIG_OUTPUT_PCB_PATH + CONFIG_OUTPUT_PCB_PREFIX + str(commit_num) + ".kicad_pcb"]    # Save in reverse numbering
+            CONFIG_OUTPUT_PCB_PATH + CONFIG_OUTPUT_PCB_PREFIX + str(commit_cnt) + ".kicad_pcb"]    # Save in reverse numbering
 
     process = subprocess.run(args=cmd, 
                             stdout=subprocess.PIPE,
@@ -74,7 +110,7 @@ for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
 
 
 # Next outputs all the X .svg files using the KiCAD CLI
-for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
+for commit_num in range(1,kicad_pcb_hashes_cnt+1):
     print("Exporting SVG from output file #" + str(commit_num) + "\n")
 ##C:\Program Files\KiCad\7.0\bin\kicad-cli pcb export svg --output C:\Users\KevinBibby\Desktop\test3.svg -l F.Cu,B.Cu --page-size-mode 2 --exclude-drawing-sheet C:\freelance\git\pt115a_vrgo-fyt-electronics-main\design\pt115a_vrgo-fyt-electronics-main.kicad_pcb
     cmd = [CONFIG_KICAD_CLI_PATH,
@@ -98,21 +134,21 @@ for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
 
 
 # Next, modifies the SVG opacity for all layers for better visual clarity
-for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
+for commit_num in range(1,kicad_pcb_hashes_cnt+1):
     print("\nSetting SVG to " + str(CONFIG_OUTPUT_IMAGE_OPACITY) + "% opacity for output file #" + str(commit_num) + "\n")
 
     # read svg file -> write png file
     process = subprocess.run([CONFIG_INKSCAPE_PATH,
-                            '--actions=select-all;object-set-property:opacity,0.70;export-overwrite;export-do;',
+                            '--actions=select-all:all;object-set-property:opacity,0.' + str(CONFIG_OUTPUT_IMAGE_OPACITY) + ';export-overwrite;export-do;',
                             CONFIG_OUTPUT_IMAGE_PATH + CONFIG_OUTPUT_IMAGE_PREFIX + str(commit_num) + ".svg"])
        
     print(process.stdout)
 
-    time.sleep(0.1) # Delay between each as sometimes hung mid loop
+    time.sleep(0.2) # Delay between each as sometimes hung mid loop
 
 
 # Next converts the X .svg to .png using inkscape. Pads the output filenames with leading zeros
-for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
+for commit_num in range(1,kicad_pcb_hashes_cnt+1):
     print("Converting SVG to PNG for output file #" + str(commit_num) + "\n")
 
     # read svg file -> write png file
@@ -127,7 +163,7 @@ for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
 
 
 # Next crops the PNGs to a specified area
-for commit_num in range(1,CONFIG_GIT_NUM_COMMITS+1):
+for commit_num in range(1,kicad_pcb_hashes_cnt+1):
     # Open image file and get size
     im = Image.open(CONFIG_OUTPUT_IMAGE_PATH + CONFIG_OUTPUT_IMAGE_PREFIX + str("%04d" % (commit_num,)) + ".png")
     img_width, img_height = im.size
